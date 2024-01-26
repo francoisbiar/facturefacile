63e2bced1f1305ded2a01ef3a1845af077fc95f3
François <francois.bari@gmail.com>
2024-01-26 15:46:22 +0100

    with viewBinding

--- app/src/main/java/com/francoisbari/facturefacile/ui/main/MainFragment.kt
+++ app/src/main/java/com/francoisbari/facturefacile/ui/main/MainFragment.kt
@@ -5,13 +5,10 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.francoisbari.facturefacile.R
 import com.francoisbari.facturefacile.data.DataPersistenceFactory
+import com.francoisbari.facturefacile.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
@@ -20,6 +17,8 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,60 +38,74 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val nbOfDaysEditText = view.findViewById<EditText>(R.id.nbOfDaysEditText)
-        val tjmEditText = view.findViewById<EditText>(R.id.tjmEditText)
-        val totalTextView = view.findViewById<TextView>(R.id.totalTextView)
-        val addDayButton = view.findViewById<Button>(R.id.addOneDayButton)
+        binding.apply {
+            nbOfDaysEditText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    // Nothing to do here
+                }
 
-        nbOfDaysEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // Nothing to do here
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+
+                override fun afterTextChanged(s: android.text.Editable?) {
+                    viewModel.setNumberOfDays(s.toString().toIntOrNull() ?: 0)
+                }
+            })
+
+            tjmEditText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    // Nothing to do here
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    // Nothing to do here
+                }
+
+                override fun afterTextChanged(s: android.text.Editable?) {
+                    viewModel.setTjm(s.toString().toIntOrNull() ?: 0)
+                }
+            })
+
+            addOneDayButton.setOnClickListener {
+                viewModel.addOneDayClicked()
             }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            viewModel.total.observe(viewLifecycleOwner) { total ->
+                totalTextView.text = total.toString()
             }
 
-            override fun afterTextChanged(s: android.text.Editable?) {
-                viewModel.setNumberOfDays(s.toString().toIntOrNull() ?: 0)
-            }
-        })
-
-        tjmEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // Nothing to do here
+            viewModel.nbOfDays.observe(viewLifecycleOwner) { nbOfDays ->
+                nbOfDaysEditText.setText(nbOfDays.toString())
             }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Nothing to do here
+            viewModel.tjm.observe(viewLifecycleOwner) { tjm ->
+                tjmEditText.setText(tjm.toString())
             }
 
-            override fun afterTextChanged(s: android.text.Editable?) {
-                viewModel.setTjm(s.toString().toIntOrNull() ?: 0)
-            }
-        })
+            viewModel.loadData()
 
-        addDayButton.setOnClickListener {
-            viewModel.addOneDayClicked()
         }
+    }
 
-        viewModel.total.observe(viewLifecycleOwner) { total ->
-            totalTextView.text = total.toString()
-        }
-
-        viewModel.nbOfDays.observe(viewLifecycleOwner) { nbOfDays ->
-            nbOfDaysEditText.setText(nbOfDays.toString())
-        }
-
-        viewModel.tjm.observe(viewLifecycleOwner) { tjm ->
-            tjmEditText.setText(tjm.toString())
-        }
-
-        viewModel.loadData()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
